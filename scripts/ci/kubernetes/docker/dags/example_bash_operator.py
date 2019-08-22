bd5efb3eafb29925ee330a40aa26f3a74ebb1947
Daniel Imberman <daniel@astronomer.io>
2019-08-22 13:02:03 -0500

    works, now cranking up number of pods

--- scripts/ci/kubernetes/docker/dags/example_bash_operator.py
+++ scripts/ci/kubernetes/docker/dags/example_bash_operator.py
@@ -53,7 +53,7 @@ run_this = BashOperator(
 
 run_this >> run_this_last
 
-for i in range(3):
+for i in range(100):
     task = BashOperator(
         task_id='runme_' + str(i),
         bash_command='echo "{{ task_instance_key_str }}" && sleep 1',
