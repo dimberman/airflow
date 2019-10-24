39eea864e76c16e23a0a6ed869d68fd00c8e1d9d
Daniel Imberman <daniel@astronomer.io>
2019-10-23 21:22:18 -0300

    add airflow_worker command

--- airflow/bin/cli.py
+++ airflow/bin/cli.py
@@ -502,6 +502,43 @@ def _run(args, dag, ti):
 
 
 @cli_utils.action_logging
+def airflow_worker(args):
+    num_workers = args.workers or 8
+    # worker_timeout = (args.worker_timeout or
+    #                   conf.get('webserver', 'web_server_worker_timeout'))
+    worker_timeout = 10000
+    hostname = args.hostname or "0.0.0.0"
+    port = args.port or "8081"
+    run_args = [
+        'gunicorn',
+        '-w', str(num_workers),
+        '-k', 'sync',
+        '-t', str(worker_timeout),
+        '-b', str(hostname) + ':' + str(port),
+        '-n', 'airflow-worker',
+        '-c', 'python:airflow.www.gunicorn_config',
+        'airflow.worker.task_runner_worker:create_app()'
+    ]
+
+    def monitor_gunicorn(gunicorn_master_proc):
+        while gunicorn_master_proc.poll() is None:
+            time.sleep(1)
+        sys.exit(gunicorn_master_proc.returncode)
+
+    def kill_proc(dummy_signum, dummy_frame):
+        gunicorn_master_proc.terminate()
+        gunicorn_master_proc.wait()
+        sys.exit(0)
+
+    gunicorn_master_proc = subprocess.Popen(run_args, close_fds=True)
+
+    signal.signal(signal.SIGINT, kill_proc)
+    signal.signal(signal.SIGTERM, kill_proc)
+
+    monitor_gunicorn(gunicorn_master_proc)
+
+
+@cli_utils.action_logging
 def run(args, dag=None):
     if dag:
         args.dag_id = dag.dag_id
@@ -2148,6 +2185,12 @@ class CLIFactory(object):
             'args': ('list_connections', 'add_connection', 'delete_connection',
                      'conn_id', 'conn_uri', 'conn_extra') + tuple(alternative_conn_specs),
         }, {
+            'func': airflow_worker,
+            'name': 'airflow_worker',
+            'help': 'starts a knative worker',
+            'args': ('port', 'workers', 'worker_timeout', 'hostname'),
+        },
+        {
             'func': create_user,
             'help': "Create an account for the Web UI (FAB-based)",
             'args': ('role', 'username', 'email', 'firstname', 'lastname',
