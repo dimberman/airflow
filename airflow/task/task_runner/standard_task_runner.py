f273c4671105857602cee994c6aca2117d2646d3
Daniel Imberman <daniel@astronomer.io>
2019-11-21 08:53:23 -0500

    ash's fixes for process spawning

--- airflow/task/task_runner/standard_task_runner.py
+++ airflow/task/task_runner/standard_task_runner.py
@@ -17,7 +17,10 @@
 # specific language governing permissions and limitations
 # under the License.
 
+import os
+
 import psutil
+from setproctitle import setproctitle
 
 from airflow.task.task_runner.base_task_runner import BaseTaskRunner
 from airflow.utils.helpers import reap_process_group
@@ -28,17 +31,38 @@ class StandardTaskRunner(BaseTaskRunner):
     Runs the raw Airflow task by invoking through the Bash shell.
     """
     def __init__(self, local_task_job):
-        super(StandardTaskRunner, self).__init__(local_task_job)
+        super().__init__(local_task_job)
+        self._rc = None
 
     def start(self):
-        self.process = self.run_command()
+        pid = os.fork()
+        if pid:
+            self.log.info("Started process %d to run task", pid)
+            self.process = psutil.Process(pid)
+            return
+        else:
+            from airflow.bin.cli import CLIFactory
+            parser = CLIFactory.get_parser()
+            args = parser.parse_args(self._command[1:])
+            setproctitle("airflow task runner {0.dag_id} {0.task_id} {0.execution_date}".format(args))
+            args.func(args)
+            os._exit(0)
 
-    def return_code(self):
-        return self.process.poll()
+    def return_code(self, timeout=0):
+        # We call this multiple times, but we can only wait on the process once.
+        if self._rc is not None:
+            return self._rc
+        try:
+            self._rc = self.process.wait(timeout=timeout)
+            self.process = None
+        except psutil.TimeoutExpired:
+            pass
+        self.log.debug("RC %r", self._rc)
+        return self._rc
 
     def terminate(self):
         if self.process and psutil.pid_exists(self.process.pid):
             reap_process_group(self.process.pid, self.log)
 
     def on_finish(self):
-        super(StandardTaskRunner, self).on_finish()
+        super().on_finish()
