9cc63fa426ba68431847f845dc61441b86e085a2
Daniel Imberman <daniel@astronomer.io>
2019-11-21 12:05:49 -0500

    airflow server using LocalTaskJob for heartbeating

--- airflow/worker/task_runner_worker.py
+++ airflow/worker/task_runner_worker.py
@@ -18,7 +18,6 @@
 # under the License.
 #
 
-import asyncio
 import logging
 import os
 from datetime import datetime
@@ -31,84 +30,56 @@ from airflow.models import (
     DagBag, TaskInstance
 )
 from airflow.utils.log.logging_mixin import (LoggingMixin)
-from airflow.utils.net import get_hostname
-from airflow.utils.state import State
-from concurrent.futures import ProcessPoolExecutor
+from airflow.jobs import LocalTaskJob
 
 import pendulum
-from contextlib import suppress
+from flask import request
+
+from flask_api import FlaskAPI
 
 app = None  # type: Any
-loop: asyncio.AbstractEventLoop = None
-pool = None
-executor = None
 DAGS_FOLDER = settings.DAGS_FOLDER
-from aiohttp import web
-
-import asyncio
-from aiohttp import web
-
-running_tasks_map = {}
-
-heartbeat_loop_task = None
-num = 0
+app = FlaskAPI(__name__)
 
 
-async def heartbeat():
-    global running_tasks_map, num
-    while True:
-        num = num + 1
-        for k, v in running_tasks_map.items():
-            a = v
-            await a.heartbeat()
-        await asyncio.sleep(1)
+@app.route("/health")
+def health():
+    name = request.args.get("name")
+    return "Hello, {}..".format(name)
 
 
-async def health(request):
-    name = request.rel_url.query.get("name", "Daniel")
-    return web.Response(text="Hello, {}. There have been {} heartbeats.".format(name, num))
-
-
-async def run_task(request):
-    global running_tasks_map
-    dag_id = request.rel_url.query['dag_id']
-    task_id = request.rel_url.query['task_id']
+@app.route("/run")
+def run_task():
+    dag_id = request.args.get('dag_id')
+    task_id = request.args.get('task_id')
     # subdir = request.rel_url.query['subdir']
-    subdir = "/root/airflow/dags"
-    response = None
-    execution_date = pendulum.fromtimestamp(int(request.rel_url.query["execution_date"]))
+    # subdir = "/root/airflow/dags"
+    subdir = None
+    execution_date = pendulum.fromtimestamp(int(request.args.get("execution_date")))
     log = LoggingMixin().log
-    log.info("running dag {} for task {} on date {} in subdir {}".format(dag_id, task_id, execution_date, subdir))
+    log.info("running dag {} for task {} on date {} in subdir {}"
+             .format(dag_id, task_id, execution_date, subdir))
     logging.shutdown()
-    key = (task_id, dag_id, execution_date).__str__()
     try:
         # IMPORTANT, have to use the NullPool, otherwise, each "run" command may leave
         # behind multiple open sleeping connections while heartbeating, which could
         # easily exceed the database connection limit when
         # processing hundreds of simultaneous tasks.
         settings.configure_orm(disable_connection_pool=True)
+
         ti = get_task_instance(dag_id=dag_id,
                                task_id=task_id,
                                subdir=subdir,
                                execution_date=execution_date)
-        out = run(ti)
-        running_tasks_map[key] = ti
-        run_task_instance(ti, log)
-
-
-        if out.state == 'success':
-            response = web.Response(
-                body="successfully ran dag {} for task {} on date {}".format(dag_id, task_id, execution_date),
-                status=200)
-        else:
-            response = web.Response(body="task failed", status=500)
+        local_job = LocalTaskJob(
+            task_instance=ti,
+        )
+        local_job.run()
+        return "task passed or failed successfully", 200
     except Exception as e:
         import traceback
         tb = traceback.format_exc()
-        response = web.Response(body="failed {} {}".format(e, tb), status=500)
-    finally:
-        running_tasks_map.pop(key, None)
-        return response
+        return "failed {} {}".format(e, tb), 500
 
 
 def process_subdir(subdir):
@@ -140,51 +111,5 @@ def get_task_instance(
     return ti
 
 
-def run(ti: TaskInstance):
-    log = LoggingMixin().log
-
-    # IMPORTANT, have to use the NullPool, otherwise, each "run" command may leave
-    # behind multiple open sleeping connections while heartbeating, which could
-    # easily exceed the database connection limit when
-    # processing hundreds of simultaneous tasks.
-    settings.configure_orm(disable_connection_pool=True)
-
-    run_task_instance(ti, log)
-    logging.shutdown()
-    return ti
-
-
-def run_task_instance(ti: TaskInstance, log):
-    ti.refresh_from_db()
-    set_task_instance_to_running(ti)
-    ti.init_run_context()
-    hostname = get_hostname()
-    log.info("Running %s on host %s", ti, hostname)
-    ti._run_raw_task()
-
-
-def set_task_instance_to_running(ti):
-    ti.state = State.RUNNING
-    session = settings.Session()
-    session.merge(ti)
-    session.commit()
-
-
-async def on_shutdown(app):
-    global heartbeat_loop_task
-    heartbeat_loop_task.cancel()
-    with suppress(asyncio.CancelledError):
-        await heartbeat_loop_task  # await for task cancellation
-
-
-async def create_app():
-    global heartbeat_loop_task
-    global loop, app, executor
-    heartbeat_loop_task = asyncio.Task(heartbeat())
-
-    loop = asyncio.get_event_loop()
-    executor = ProcessPoolExecutor()
-    loop.set_default_executor(executor)
-    app = web.Application()
-    app.add_routes([web.get('/health', health), web.get('/run', run_task)])
+def create_app():
     return app
--- tests/worker/test_task_runner_worker.py
+++ tests/worker/test_task_runner_worker.py
@@ -18,69 +18,41 @@
 # under the License.
 #
 
-from airflow.models import DAG, TaskInstance as TI
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.utils import timezone
-from airflow.utils.db import create_session
-from airflow.models import taskinstance
-from airflow.utils.state import State
-from tests.models import DEFAULT_DATE
 from airflow.worker import task_runner_worker
-import time
-import asyncio
-from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop
+import unittest
 
 
-class TestTaskRunnerWorker(AioHTTPTestCase):
+class TestTaskRunnerWorker(unittest.TestCase):
+    def setUp(self) -> None:
+        app = task_runner_worker.create_app()
+        self.app = app.test_client()
 
-    async def get_application(self):
-        """
-        Override the get_app method to return your application.
-        """
+    def test_hello(self):
+        resp = self.app.get("health", query_string={'name': 'daniel'})
+        self.assertEqual(resp.status_code, 200)
 
-        app = await task_runner_worker.create_app()
-        return app
-
-    @unittest_run_loop
-    async def test_hello(self):
-        resp = await self.client.request("GET", "health?name=daniel")
-        dag_id='test_requeue_over_dag_concurrency'
-        task_id = 'test_requeue_over_dag_concurrency_op'
-        dag = DAG(dag_id=dag_id, start_date=DEFAULT_DATE,
-                  max_active_runs=1, concurrency=2)
-        task = DummyOperator(task_id=task_id, dag=dag)
+    def test_passing_task(self):
+        dag_id = 'example_python_operator'
+        task_id = 'print_the_context'
         import datetime
         start_date = datetime.datetime(year=2019, day=1, month=1)
-        ti = TI(task=task, execution_date=start_date, state=State.RUNNING)
+        exec_date = int(datetime.datetime.timestamp(start_date))
 
-        task_runner_worker.running_tasks_map["test_requeue_over_dag_concurrency_op"] = ti
-        await asyncio.sleep(5)
-        text = await resp.text()
+        resp = self.app.get("run", query_string={"task_id": task_id,
+                                                 "dag_id": dag_id,
+                                                 "execution_date": exec_date}
+                            )
+        self.assertEqual(200, resp.status_code, resp.data)
 
-        print(text)
-
-    @unittest_run_loop
-    async def test_heartbeat_function(self):
-        dag_id='test_requeue_over_dag_concurrency'
-        task_id = 'test_requeue_over_dag_concurrency_op'
-        dag = DAG(dag_id=dag_id, start_date=DEFAULT_DATE,
-                  max_active_runs=1, concurrency=2)
-        task = DummyOperator(task_id=task_id, dag=dag)
+    def test_failing_task(self):
+        dag_id = 'example_pythxxxxon_operator'
+        task_id = 'print_the_context'
         import datetime
         start_date = datetime.datetime(year=2019, day=1, month=1)
-        ti = TI(task=task, execution_date=start_date, state=State.RUNNING)
+        exec_date = int(datetime.datetime.timestamp(start_date))
 
-        with create_session() as session:
-            session.query(TI).filter(
-                TI.dag_id == dag_id,
-                TI.task_id == task_id,
-            ).delete()
-            session.commit()
-            stale_time = timezone.utcnow()- datetime.timedelta(seconds=20)
-            ti.heartbeat(session=session, time=stale_time)
-            stale = taskinstance.get_stale_running_task_instances(session, stale_tolerance=2)
-            self.assertNotEqual(stale, [])
-            task_runner_worker.running_tasks_map["test_requeue_over_dag_concurrency_op"] = ti
-            await asyncio.sleep(3)
-            stale = taskinstance.get_stale_running_task_instances(session, stale_tolerance=5)
-            self.assertEqual(stale, [])
+        resp = self.app.get("run", query_string={"task_id": task_id,
+                                                 "dag_id": dag_id,
+                                                 "execution_date": exec_date}
+                            )
+        self.assertEqual(500, resp.status_code)
